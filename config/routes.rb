caa60c99f9606525211500fac1a3a163bcbf9270
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-12-05 13:40:24 +0000

    [Add]routing記載

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,19 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get 'homes/about'
+    get 'homes/help'
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :recruitings
+    resources :areas, only: [:index, :show, :edit, :update]
   end
   
+  
+  # 管理側
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :prefectures, only: [:index, :create, :edit, :update]
+    resources :recruitings, only: [:index, :show, :edit, :update, :delete]
+  end
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
