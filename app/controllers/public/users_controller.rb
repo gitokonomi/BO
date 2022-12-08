d704cb221e4249ae5d610d03572e0df024a9a75d
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-12-08 07:43:34 +0000

    [Add]一覧ページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,27 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!  
+  
   def index
+    @users = User.page(params[:page]).per(15)
   end
 
   def show
+    @user = User.find(params[:id])    
   end
 
   def edit
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update
   end
+
+
+  
+private
+  def user_params
+    params.require(:user).permit(:name,:email,:introduction)
+  end
+  
 end
