ccdb9785932e790bb5984ca288b1ab34dd9b4b00
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-12-08 17:14:21 +0000

    [Add]編集機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,7 @@
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!  
-  
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def index
     @users = User.page(params[:page]).per(15)
   end
@@ -11,18 +12,31 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @prefectures = Prefecture.all
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update
+    # @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "変更を保存しました。"
+    else
+      render "edit"
+    end
   end
 
 
-  
+
 private
   def user_params
-    params.require(:user).permit(:name,:email,:introduction)
+    params.require(:user).permit(:name, :email, :introduction, :profile_image)
   end
-  
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
 end
