55e93e49119b10094a599f6f08f023cb9718173a
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-12-25 13:40:06 +0000

    [Update]areaフラッシュ

--- app/controllers/public/areas_controller.rb
+++ app/controllers/public/areas_controller.rb
@@ -1,11 +1,11 @@
 class Public::AreasController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @area = current_user.areas.new(area_params)
     # 既に登録済みのエリアを選んだ場合の挙動
     if current_user.areas.find_by(prefecture_id: params[:area][:prefecture_id]).present?
-      redirect_to edit_user_path(current_user), notice: "既に追加済みです。"
+      redirect_to edit_user_path(current_user), alert: "既に追加済みです。"
     elsif @area.save
       @areas = current_user.areas.all
       redirect_to edit_user_path(current_user), notice: "追加しました。"
@@ -13,13 +13,13 @@ class Public::AreasController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
   def destroy
     @area = Area.find(params[:id])
     @area.destroy
     redirect_to edit_user_path(current_user)
   end
-  
+
 
 
 
@@ -29,5 +29,5 @@ private
   def area_params
     params.require(:area).permit(:prefecture_id)
   end
-  
+
 end
