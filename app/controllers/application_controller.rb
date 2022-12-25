b2413ff06063bdd72ff2cacbf13f40b7b0109ec1
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-12-25 16:46:31 +0000

    [Update]管理者サインイン後遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
       when Admin
-        admin_users_path
+        admin_recruitings_path
       when User
         recruitings_path
     end
