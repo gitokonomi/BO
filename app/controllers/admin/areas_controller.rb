12db1b4218c1fb0bab7014b5447e2ae762410341
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2023-01-02 14:18:27 +0000

    [Fix]管理者ユーザー編集エリア部分

--- app/controllers/admin/areas_controller.rb
+++ app/controllers/admin/areas_controller.rb
@@ -1,24 +1,24 @@
 class Admin::AreasController < ApplicationController
   
   def create
-    # @user = User.find(params[:id])
-    @area = @user.areas.new(area_params)
+    @area = Area.new(area_params)
+    user = @area.user
     # 既に登録済みのエリアを選んだ場合の挙動
-    if @user.areas.find_by(prefecture_id: params[:area][:prefecture_id]).present?
-      redirect_to admin_edit_user_path(@user), alert: "既に追加済みです。"
+    if Area.find_by(prefecture_id: @area.prefecture_id, user_id: @area.user_id).present?
+      redirect_to edit_admin_user_path(user), alert: "既に追加済みです。"
     elsif @area.save
-      @areas = @user.areas.all
-      redirect_to admin_edit_user_path(@user), notice: "追加しました。"
+      @areas = @area.user.areas.all
+      redirect_to edit_admin_user_path(user), notice: "追加しました。"
     else
       redirect_to root_path
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @area = Area.find(params[:id])
+    user = @area.user
     @area.destroy
-    redirect_to admin_edit_user_path(@user)
+    redirect_to edit_admin_user_path(user), notice: "削除しました。"
   end
 
 
@@ -28,7 +28,7 @@ class Admin::AreasController < ApplicationController
 private
 
   def area_params
-    params.require(:area).permit(:prefecture_id)
+    params.require(:area).permit(:prefecture_id, :user_id)
   end
 
 end
